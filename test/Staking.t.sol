b1e2f5bc686352d0fbb929e521837e91fdac6860
simon okwa <simoneth1996@gmail.com>
2025-09-24 06:26:37 +0100

    Staking foundry test

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.26;
 
-import  "forge-std/Test.sol";
-import {StakingRewards, IERC20} from "src/Staking.sol";
+import "forge-std/Test.sol";
+import {StakingRewards, IERC20} from "src/StakingRewards.sol";
 import {MockERC20} from "test/MockErc20.sol";
 
 contract StakingTest is Test {
@@ -22,7 +22,7 @@ contract StakingTest is Test {
         vm.stopPrank();
     }
 
-    function test_alwaysPass() public {
+    function test_alwaysPass() public view {
         assertEq(staking.owner(), owner, "Wrong owner set");
         assertEq(address(staking.stakingToken()), address(stakingToken), "Wrong staking token address");
         assertEq(address(staking.rewardsToken()), address(rewardToken), "Wrong reward token address");
@@ -53,7 +53,7 @@ contract StakingTest is Test {
         assertEq(staking.totalSupply(), _totalSupplyBeforeStaking + 5e18, "totalsupply didnt update correctly");
     }
 
-    function  test_cannot_withdraw_amount0() public {
+    function test_cannot_withdraw_amount0() public {
         vm.prank(bob);
         vm.expectRevert("amount = 0");
         staking.withdraw(0);
@@ -67,7 +67,6 @@ contract StakingTest is Test {
         staking.withdraw(2e18);
         assertEq(staking.balanceOf(bob), userStakebefore - 2e18, "Balance didnt update correctly");
         assertLt(staking.totalSupply(), totalSupplyBefore, "total supply didnt update correctly");
-
     }
 
     function test_notify_Rewards() public {
@@ -76,37 +75,91 @@ contract StakingTest is Test {
         staking.setRewardsDuration(1 weeks);
 
         // simulate owner calls setReward successfully
-        vm.prank(owner);
+        vm.startPrank(owner);
         staking.setRewardsDuration(1 weeks);
         assertEq(staking.duration(), 1 weeks, "duration not updated correctly");
         // log block.timestamp
         console.log("current time", block.timestamp);
-        // move time foward 
+        // move time foward
         vm.warp(block.timestamp + 200);
-        // notify rewards 
+        // notify rewards
         deal(address(rewardToken), owner, 100 ether);
-        vm.startPrank(owner); 
+        vm.startPrank(owner);
         IERC20(address(rewardToken)).transfer(address(staking), 100 ether);
-        
+
         // trigger revert
         vm.expectRevert("reward rate = 0");
         staking.notifyRewardAmount(1);
-    
+
         // trigger second revert
         vm.expectRevert("reward amount > balance");
         staking.notifyRewardAmount(200 ether);
 
         // trigger first type of flow success
         staking.notifyRewardAmount(100 ether);
-        assertEq(staking.rewardRate(), uint256(100 ether)/uint256(1 weeks));
+        assertEq(staking.rewardRate(), uint256(100 ether) / uint256(1 weeks));
         assertEq(staking.finishAt(), uint256(block.timestamp) + uint256(1 weeks));
         assertEq(staking.updatedAt(), block.timestamp);
-    
+
         // trigger setRewards distribution revert
         vm.expectRevert("reward duration not finished");
         staking.setRewardsDuration(1 weeks);
-    
+        vm.stopPrank();
     }
 
+    function test_getReward_0() public {
+        vm.startPrank(owner);
+        uint256 currentReward = staking.rewards(owner);
+        assertEq(currentReward, 0, "Rewards can't be greater than zero before staking");
+        vm.stopPrank();
+    }
 
+    function test_getReward_not0() public {
+        test_can_stake_successfully();
+        uint256 initialRewardEarned = staking.earned(bob);
+
+        deal(address(rewardToken), owner, 100 ether);
+
+        vm.startPrank(owner);
+        rewardToken.transfer(address(staking), 100 ether);
+        staking.setRewardsDuration(1 weeks);
+
+        staking.notifyRewardAmount(100 ether);
+
+        vm.stopPrank();
+        vm.warp(block.timestamp + 5 days);
+
+        uint256 newRewardEarned = staking.earned(bob);
+
+        assertGt(newRewardEarned, initialRewardEarned, "Reward earned not updated accordinly");
+    }
+
+    function test_reward_per_token_0() public view {
+        uint256 rewardPerToken = staking.rewardPerToken();
+
+        assertEq(rewardPerToken, 0, "Issues with reward per token update");
+    }
+
+    function test_reward_per_token_not_0() public {
+        test_can_stake_successfully();
+        deal(address(rewardToken), owner, 200 ether);
+        vm.startPrank(owner);
+        rewardToken.transfer(address(staking), 100 ether);
+        staking.setRewardsDuration(1 weeks);
+
+        staking.notifyRewardAmount(100 ether);
+
+        vm.stopPrank();
+        vm.warp(block.timestamp + 5 days);
+
+        uint256 reward_rate_ = staking.rewardRate();
+        uint256 reward_stored = staking.rewardPerTokenStored();
+        uint256 total_supply_ = staking.totalSupply();
+        uint256 updated_at_ = staking.updatedAt();
+        uint256 last_time = staking.lastTimeRewardApplicable();
+        uint256 reward_per_token = reward_stored + (last_time - updated_at_) * 1e18 / total_supply_;
+
+        assertGt(reward_rate_, 0, "Reward rate failed to update");
+        assertGt(reward_per_token, 0, "There's a problem with reward per token calculation");
+    }
 }
